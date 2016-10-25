9cd4511e55d624716fcc140320057296e3d5cf7b
Eric Mickelsen <mickelsen.eric@gmail.com>
2016-10-24 20:29:32 -0700

    Forget using cloudfront for now
    We need a better solution for assets in general
    
    Revert "api cors and max-age"
    
    This reverts commit 79b57634ebfdc767f63c5182076fed626d6d3ec9.
    
    Revert "basic cloudfront usage"
    
    This reverts commit b906e927420a9f54ad7e117c416e43472f98dc00.
    
    Conflicts:
    	client/index.html.js

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ const app = koa();
 const server = require('http').createServer(app.callback());
 const bodyParser = require('koa-body-parser');
 const session = require('koa-generic-session');
-const router = require('koa-router')({});
-const api = require('koa-router')({});
+const router = require('koa-router')({
+    prefix: '/api'
+});
 const serve = require('koa-static');
 const io = require('socket.io')(server);
 const peering = new (require('./Peering.js'))(io.of('peering'));
@@ -12,43 +13,21 @@ const peering = new (require('./Peering.js'))(io.of('peering'));
 const isProd = process.env.NODE_ENV === 'production';
 const port = isProd ? 80 : 3000;
 
-const html = require('../client/index.html.js').template();
-router.get('/', function *(next) {
-    this.type = 'text/html';
-    this.body = html;
-});
-
-function *cacheControl(next) {
-    // 86400000 = a day in ms
-    this.set('Cache-Control', 'max-age=86400000'); 
-    yield next;
-}
-
 const generateMap = require('./generateMap');
-api.get('/map/:id', function *(next) {
+router.get('/map/:id', function *(next) {
     this.type = 'image/png';
     this.body = generateMap(this.params.id, true);
 });
 
-api.get('/texture/:id', function *(next) {
+router.get('/texture/:id', function *(next) {
     this.type = 'image/png';
     this.body = generateMap(this.params.id, false);
 });
-api.use(function *(next) {
-    // 86400000 = a day in ms
-    this.set('Cache-Control', 'max-age=86400000');
-    this.set('Access-Control-Allow-Origin', '*');
-    yield next;
-});
-
-router.use('/api', api.routes(), api.allowedMethods());
 
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
-app.use(serve(`${__dirname}/../dist`, {
-    maxage: 300000 // 5 minutes in ms
-}));
+app.use(serve(`${__dirname}/../dist`));
 
 server.listen(port);
 console.log(`Server started on http://${require("os").hostname()}:${port}`);
